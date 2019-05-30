8317a9496cfbc38b5e14eaf45072cac409f5f0b3
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2019-05-30 12:02:39 +0200

    Account

---
+++ Sentje/database/factories/BankAccountFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\BankAccount;
+use Faker\Generator as Faker;
+
+$factory->define(BankAccount::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
